69c22b56858080dfcfd3132a5da27f6e6842de0d
BapNesS <carlier.baptiste@gmail.com>
2021-11-05 18:07:12 +0100

    [Lib] Init unit test

--- lib-client/src/test/java/com/baptistecarlier/kotlin/datagouvfr/client/ExampleUnitTest.kt
+++ lib-client/src/test/java/com/baptistecarlier/kotlin/datagouvfr/client/ExampleUnitTest.kt
@@ -1,8 +1,5 @@
 package com.baptistecarlier.kotlin.datagouvfr.client
 
-import com.baptistecarlier.kotlin.datagouvfr.client.model.*
-import io.ktor.util.date.*
-import kotlinx.datetime.*
 import org.junit.Assert
 import org.junit.Test
 
@@ -14,20 +11,8 @@ import org.junit.Test
 class ExampleUnitTest {
     @Test
     fun tmp_test() {
-        val currentMoment: Instant = Clock.System.now()
-        val datetimeInUtc: LocalDateTime = currentMoment.toLocalDateTime(TimeZone.UTC)
-
-        val item = Dataset(
-            createdAt = datetimeInUtc,
-            description = "String",
-            frequency = Dataset.FrequencyEnum.ANNUAL,
-            lastModified = datetimeInUtc,
-            lastUpdate = datetimeInUtc,
-            page = "String",
-            slug = "String",
-            title = "String",
-            uri = "String",
-
+        val item = DgfrService(
+            apiKey = "fake"
         )
         Assert.assertNotNull(item)
     }
